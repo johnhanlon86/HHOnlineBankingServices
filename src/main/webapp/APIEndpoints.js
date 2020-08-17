5932d9c7222b447962150fc4eb9ea19bfb2c6666
DarylHowe <howedaryl@hotmail.com>
2020-08-17 16:22:43 +0100

    Web front end bug

--- src/main/webapp/APIEndpoints.js
+++ src/main/webapp/APIEndpoints.js
@@ -4,14 +4,15 @@
 // Allow Blocked content... if you see that warning in the browser
 
 
-$(document).ready(function() {
-    $.ajax({
-        url: "http://localhost:49000/api/bank/getaccountsortcode/0/41324/darylhowe"
-    }).then(function(data) {
-       $('.resp-sortCode').append(data);
-       console.log(data)
-    });
-});
+
+//$(document).ready(function() {
+//    $.ajax({
+//        url: "http://localhost:49000/api/bank/getaccountsortcode/0/41324/darylhowe"
+//    }).then(function(data) {
+//       $('.resp-sortCode').append(data);
+//       console.log(data)
+//    });
+//});
 
 
 
