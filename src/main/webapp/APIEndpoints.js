c9445ca87bcb1e1eb2b4eedde29e9cee0693c145
DarylHowe <howedaryl@hotmail.com>
2020-08-17 18:38:09 +0100

    Transaction HTML JS added

--- src/main/webapp/APIEndpoints.js
+++ src/main/webapp/APIEndpoints.js
@@ -66,11 +66,35 @@ axios.get('http://localhost:49000/api/bank/getaccountdetails/0/41324/darylhowe')
             '<div> Account Number: ' + resp.data.accountNumber + '</div>' + 
             '<div> Sort Code: ' + resp.data.sortCode + '</div>' +
             '<div> Account Type: ' + resp.data.accountType + '</div>' + 
-            '<div> Balance: ' + resp.data.accountBalance + '</div>'  
+            '<div> Balance: ' + resp.data.balance + '</div>'  
 });
 }
 
+function viewDarylAccountHistory83232() {
 
+axios.get('http://localhost:49000/api/bank/getaccounthistory/0/83232/darylhowe')
+                    .then(function (response) {
+                    document.getElementById('people').innerHTML = response.data.map(function (transaction) {
+                    return (
+                            '<div class = "customerContainer">' +
+                            '<div> Transaction Id: ' + transaction.transactionId + '</div>' +
+                            '<div> Date: ' + transaction.transactionDate + '</div>' +
+                            '<div> Description: ' + transaction.description + '</div>' +
+                            '<div> Account Type: ' + transaction.accountType + '</div>' +
+                            '<div> Amount: ' + transaction.transactionAmount + '</div>' +
+                            '<div> Pre-Transaction Balance: ' + transaction.preTransactionBalance + '</div>' +
+                            '<div> Post-Transaction Balance: ' + transaction.postTransactionBalance + '</div>' +
+                            '<div> Status: ' + transaction.status + '</div>' +
+                            '<div> Transaction Type: ' + transaction.transactionType + '</div>' +
+                            '<div> -------------------------------- </div>' +
+                            '</div>'
+                            );
+                            }).join('');
+                            })
+                            .catch(function (err) {
+                            document.getElementById('people').innerHTML = '<li class="text-danger">' + err.message + '</li>';
+                            });
+}
 
 
 
